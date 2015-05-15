2e89905a89e198897515c37f09bd269a15274583
Daniel Lovera <daniel.lovera@zenika.com>
2015-05-15 14:45:57 +0200

    Mise à jour du tp0 pour utilisation d'un archetype maven-archetype-osgi-service fourni (dérivé du maven-archetype-osgi-service de OPS4J), ajout du repository provisionné (à extraire en préambule de tp0)

---
+++ TPs/resources/tp0/maven-archetype-osgi-service/src/main/resources/archetype-resources/src/test/java/internal/ExampleServiceImplTest.java
@@ -0,0 +1,24 @@
+package ${package}.internal;
+
+import java.util.Arrays;
+import org.junit.Assert;
+import junit.framework.TestCase;
+import ${package}.ExampleService;
+
+public class ExampleServiceImplTest extends TestCase {
+
+    public void testExampleServiceScramble() {
+        ExampleService anExampleService = new ExampleServiceImpl();
+
+        String in = "This is a test of the text scrambling service";
+        String out = anExampleService.scramble(in);
+
+        char[] inChars = in.toCharArray();
+        char[] outChars = out.toCharArray();
+
+        Arrays.sort(inChars);
+        Arrays.sort(outChars);
+
+        Assert.assertEquals("Uses same letters", new String(inChars), new String(outChars));
+    }
+}
